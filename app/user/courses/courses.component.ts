984d51eff615d30e02b88596537d4ed261d415bf
pranaysashank <pranaysashank@gmail.com>
2016-09-09 15:52:17 +0530

    attendance in a table

---
+++ app/user/courses/courses.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Pranay Sashank on 9/7/2016.
+ */
+
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'courses',
+    templateUrl: 'app/user/courses/courses.component.html'
+})
+export class CoursesComponent {}
---
+++ app/user/posts/posts.service.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Pranay Sashank on 9/7/2016.
+ */
+
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class PostsService {
+
+}
